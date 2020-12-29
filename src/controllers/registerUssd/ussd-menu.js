84cb03012f4d7d00b6593dedd6571bd15de7b27e
Naimaoye <naimatoyewale@gmail.com>
2020-12-29 23:35:16 +0100

    new commit

--- src/controllers/registerUssd/ussd-menu.js
+++ src/controllers/registerUssd/ussd-menu.js
@@ -1,5 +1,5 @@
 import qs from 'qs';
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 import Report from '../../models/report';
 import {
@@ -36,51 +36,48 @@ const password = 'test';
 
 export default class Ussd {
     static async registerUssdDetails(res, req) {
-        try {
-                const queryString = req.getQuery();
-                const parseUrl = qs.parse(queryString);
-                console.log('incoming req', parseUrl);
-                const { id, msisdn, smsc, shortcode, text } = parseUrl;
-
-                // ussd question number
-                let ussdQuestion = 1;
-
-                //back option
-                const isBackOption = '00' || '';
-                // convert text value to float.
-
-                // sessionId to identify user performing registration
-                if(msisdn == '2349154100054' || msisdn == '2347058793298'){
-                const ussdIdExist = await Report.findOne({ phoneNumber: msisdn,  ussdID: id });
-                if (ussdIdExist) {
-                    ussdQuestion = ussdIdExist.ussdQuestionNumber;
+        const queryString = req.getQuery();
+        const parseUrl = qs.parse(queryString);
+        console.log('incoming req', parseUrl);
+        const { msisdn, smsc, shortcode } = parseUrl;
+        if(msisdn == '2349154100054' || msisdn == '2347058793298'){
+            axios.get('http://10.0.0.56:13150/cgi-bin/sendsms', {
+                params: {
+                'username': username,
+                'password': password,
+                'from': shortcode,
+                'smsc': smsc,
+                'to': msisdn,
+                'text': GENDER_SELECTION,
+                'meta-data': '?smpp?meta-data=2'
                 }
-                if (ussdQuestion === 1) {
-                    // first state, before the user selects an option
-                    if (text.includes('#')){
-                        const requestUrl = `http://10.0.0.56:13150/cgi-bin/sendsms?username=${username}&password=${password}&from=${shortcode}&smsc=${smsc}&to=${msisdn}&text=${GENDER_SELECTION}&meta-data=?smpp?meta-data=2&`
-                        fetch(requestUrl)
-                        .then(res => res.json())
-                        .then(json => console.log(json))
-                        .catch(err => console.log(err));
-                        const newReport = new Report({
-                            ussdID: id,
-                            phoneNumber: msisdn
-                        });
-                        await newReport.save();
-                    }
-            }
+            })
+            .then(function (response) {
+            console.log("resp",response);
+            })
+            .catch(function (error) {
+            console.log('err',error);
+            });
         } else {
-            const requestUrl = `http://10.0.0.56:13150/cgi-bin/sendsms?username=${username}&password=${password}&from=${shortcode}&smsc=${smsc}&to=${msisdn}&text=${ERROR_MESSAGE}&meta-data=?smpp?meta-data=16&`
-                        fetch(requestUrl)
-                        .then(res => res.json())
-                        .then(json => console.log(json))
-                        .catch(err => console.log(err));
-        }
-        res.end('');
-        } catch(error){
-            res.send(ERROR_MESSAGE);
-        }
+            axios.get('http://10.0.0.56:13150/cgi-bin/sendsms', {
+                params: {
+                'username': username,
+                'password': password,
+                'from': shortcode,
+                'smsc': smsc,
+                'to': msisdn,
+                'text': '',
+                'meta-data': '?smpp?meta-data=16'
+                }
+            })
+            .then(function (response) {
+            console.log("resp",response);
+            })
+            .catch(function (error) {
+            console.log('err',error);
+            });
+            }
+        res.end();
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,79 +1,13 @@
 import { App } from '@sifrr/server';
-import qs from 'qs';
-import axios from 'axios';
-//import fetch from 'node-fetch';
-// import Ussd from './controllers/registerUssd/ussd-menu'
-import {
-  SUCCESS_MESSAGE,
-  ERROR_MESSAGE,
-  INVALID_CODE,
-  GENDER_SELECTION,
-  INCIDENCE_SELECTION,
-  STATE_ALPHABET_SELECTION,
-  STATE_SELECTION_PAGE1,
-  STATE_SELECTION_PAGE2,
-  STATE_SELECTION_PAGE3,
-  STATE_SELECTION_PAGE4,
-  STATE_SELECTION_PAGE5,
-} from './controllers/registerUssd/constants';
-
-const username = 'test';
-const password = 'test';
+import Ussd from './controllers/registerUssd/ussd-menu';
 
 const app = new App();
 
 const port = 3003;
 
-// parse request url coming from kannel
-// give it an empty reply once the user connects 
-// parse the url params
-// console.log something once the request is made
 
-app.get('/', (res, req) => {
-  const queryString = req.getQuery();
-                const parseUrl = qs.parse(queryString);
-                console.log('incoming req', parseUrl);
-                const { msisdn, smsc, shortcode } = parseUrl;
-  //const requestUrl = `http://10.0.0.56:13150/cgi-bin/sendsms?username=${username}&password=${password}&from=${shortcode}&smsc=${smsc}&to=${msisdn}&text=${GENDER_SELECTION}&meta-data=%3Fsmpp%3Fmeta-data%3D2`
-  if(msisdn == '2349154100054' || msisdn == '2347058793298'){
-  axios.get('http://10.0.0.56:13150/cgi-bin/sendsms', {
-    params: {
-      'username': username,
-      'password': password,
-      'from': shortcode,
-      'smsc': smsc,
-      'to': msisdn,
-      'text': GENDER_SELECTION,
-      'meta-data': '?smpp?meta-data=2'
-    }
-  })
-  .then(function (response) {
-    console.log("resp",response);
-  })
-  .catch(function (error) {
-    console.log('err',error);
-  });
-} else {
-  axios.get('http://10.0.0.56:13150/cgi-bin/sendsms', {
-    params: {
-      'username': username,
-      'password': password,
-      'from': shortcode,
-      'smsc': smsc,
-      'to': msisdn,
-      'text': 'invalid code',
-      'meta-data': '?smpp?meta-data=16'
-    }
-  })
-  .then(function (response) {
-    console.log("resp",response);
-  })
-  .catch(function (error) {
-    console.log('err',error);
-  });
-}
-  res.end();
-}).listen(port, token => {
+app.get('/', Ussd.registerUssdDetails)
+.listen(port, token => {
   token ?
   console.log(`Listening to port ${port}`) :
   console.log(`Failed to listen to port ${port}`);

6f686947e6bcaefe753eb390269d30009bea9fbd
Naimat Oyewale <naimatoyewale@gmail.com>
2021-04-17 11:48:42 +0100

    new commit

--- src/utils/redis-config.js
+++ src/utils/redis-config.js
@@ -2,23 +2,23 @@ import redis from 'redis';
 
 const client = redis.createClient(6379);
 client.on("connect", () => {
-   //console.error("connected to redis...");
+   console.error("connected to redis...");
  });
 
  client.on("ready", () => {
-    //console.error("connected to redis and ready to use");
+    console.error("connected to redis and ready to use");
  });
 
  client.on("error", (error) => {
-    //console.error("redis error", error.message);
+    console.error("redis error", error.message);
  });
 
  client.on("end", () => {
-    //console.error("client disconnected");
+    console.error("client disconnected");
  });
 
  client.on("ready", () => {
-    //console.error("connected to redis and ready to use");
+    console.error("connected to redis and ready to use");
  });
 
  export default client;
